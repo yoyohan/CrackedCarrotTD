6aae8924b7c55bb7736f80375f7706561042e60c
Johan Elgered <elgered@student.chalmers.se>
2010-02-15 14:55:56 +0100

    Inserted main menu code

---
+++ src/com/crackedcarrot/menu/Credits.java
@@ -0,0 +1,16 @@
+package com.crackedcarrot.menu;
+
+import com.crackedcarrot.menu.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Credits extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credits);      
+              
+    }
+}
---
+++ src/com/crackedcarrot/menu/MainMenu.java
@@ -0,0 +1,56 @@
+package com.crackedcarrot.menu;
+
+import com.crackedcarrot.menu.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainMenu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button StartGameButton = (Button)findViewById(R.id.StartGame);
+        StartGameButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent StartGame = new Intent(MainMenu.this,StartGame.class);
+        		startActivity(StartGame);
+        	}
+        });
+        
+        Button OptionsButton = (Button)findViewById(R.id.Options);
+        OptionsButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent Options = new Intent(MainMenu.this,Options.class);
+        		startActivity(Options);
+        	}
+        });
+        
+        Button HelpButton = (Button)findViewById(R.id.Help);
+        HelpButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent Help = new Intent(MainMenu.this,Help.class);
+        		startActivity(Help);
+        	}
+        });
+        
+        Button CreditsButton = (Button)findViewById(R.id.Credits);
+        CreditsButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent Credits= new Intent(MainMenu.this,Credits.class);
+        		startActivity(Credits);
+        	}
+        });
+        
+    }
+}
