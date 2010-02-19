d2e9f21cf66f7fffaf96121960c53d9f4fb5f31a
Johan Elgered <elgered@student.chalmers.se>
2010-02-19 11:22:33 +0100

    Implemented direction for Creature. A creature moves only in four directions.

--- src/com/crackedcarrot/Creature.java
+++ src/com/crackedcarrot/Creature.java
@@ -8,11 +8,18 @@ public class Creature extends Sprite{
 	
     
     // A creatures health
-    int health;
+    public int health;
     // The next way point for a given creature
     public int nextWayPoint;
     // The speed of the creature
     public float velocity;
+    // The different directions for a creature
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    public static final int UP = 2;
+    public static final int DOWN = 3;
+    // The current direction of the creature
+    public int direction;
 	
     
 	public Creature(int resourceId){
