57fa56ca343c0bd74f3d1e959b4c169cf22209d4
Fredrik <wistrom@student.chalmers.se>
2010-03-27 00:09:51 +0100

    GUI .xml thingie done.

--- src/com/crackedcarrot/menu/Options.java
+++ src/com/crackedcarrot/menu/Options.java
@@ -1,19 +1,49 @@
 package com.crackedcarrot.menu;
 
 import com.crackedcarrot.menu.R;
-
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
 
 public class Options extends Activity {
-    /** Called when the activity is first created. */
+
+	private boolean playSound = true;
+
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.options);      
+        setContentView(R.layout.mainmenu_options);
           
         /** Ensures that the activity is displayed only in the portrait orientation */
     	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    	
+        ImageButton imageButton1 = (ImageButton) findViewById(R.id.MainMenuOptionsImageButton1);
+        imageButton1.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		Log.d("OPTIONS", "Clicked button 1");
+        	}
+        });
+
+        ImageButton imageButton2 = (ImageButton) findViewById(R.id.MainMenuOptionsImageButton2);
+        imageButton2.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		Log.d("OPTIONS", "Clicked button 2");
+        	}
+        });
+        
+        ImageButton imageButtonSave = (ImageButton) findViewById(R.id.MainMenuOptionsImageButtonSave);
+        imageButtonSave.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		Log.d("OPTIONS", "Clicked button Save");
+        	}
+        });
+        
     }
 }
