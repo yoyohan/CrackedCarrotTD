86075d7631efca60848dd5e5062a96eb11dcb99b
Albin Andersson <albin.andersson@gmail.com>
2010-04-22 14:00:46 +0200

    Small cleanup

--- src/com/crackedcarrot/multiplayer/MultiplayerService.java
+++ src/com/crackedcarrot/multiplayer/MultiplayerService.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import com.example.android.BluetoothChat.BluetoothChatService.ConnectedThread;
-
-
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
