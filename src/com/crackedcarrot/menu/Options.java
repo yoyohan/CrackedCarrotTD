29f20e20a492cfb310ab4c73125b0a3041c63c1e
Fredrik <wistrom@student.chalmers.se>
2010-04-30 13:07:29 +0200

    More changes to Options dialog

--- src/com/crackedcarrot/menu/Options.java
+++ src/com/crackedcarrot/menu/Options.java
@@ -8,11 +8,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class Options extends Activity {
 
@@ -22,7 +20,7 @@ public class Options extends Activity {
 	
 	private ImageButton imageButton1;
 	private ImageButton imageButton2;
-	private ImageButton imageButton4;
+	private ImageButton imageButton3;
 
     private ScoreNinjaAdapter scoreNinjaAdapter;
 
@@ -41,7 +39,7 @@ public class Options extends Activity {
     	
         // Restore preferences
         SharedPreferences settings = getSharedPreferences("Options", 0);
-        optionsHighscore = settings.getBoolean("optionsHighscore", false);
+        optionsHighscore = settings.getBoolean("optionsHighscore", true);
         optionsNextLevel = settings.getBoolean("optionsNextLevel", true);
         optionsSound     = settings.getBoolean("optionsSound", true);
         
@@ -76,17 +74,9 @@ public class Options extends Activity {
         setHighscore(optionsHighscore);
 
         
-        ImageButton imageButton3 = (ImageButton) findViewById(R.id.MainMenuOptionsImageButton3);
-        imageButton3.setOnClickListener(new OnClickListener() {
-        	public void onClick(View v) {
-        			// Shows the highscore.
-        		scoreNinjaAdapter.show();
-        	}
-        });
-        
         	// Enable/disable NextLevel-dialog.
-        imageButton4 = (ImageButton) findViewById(R.id.MainMenuOptionsImageButton4);
-        imageButton4.setOnClickListener(new OnClickListener() {
+        imageButton3 = (ImageButton) findViewById(R.id.MainMenuOptionsImageButton3);
+        imageButton3.setOnClickListener(new OnClickListener() {
         	public void onClick(View v) {
         			// Toggles highscore on or off
         		if (optionsNextLevel) {
@@ -134,9 +124,9 @@ public class Options extends Activity {
     	this.optionsHighscore = b;
     	
     	if (b) {
-			imageButton2.setImageResource(R.drawable.button_highscore_on);
+			imageButton2.setImageResource(R.drawable.button_yes);
     	} else {
-			imageButton2.setImageResource(R.drawable.button_highscore_off);
+			imageButton2.setImageResource(R.drawable.button_no);
     	}
     }
     
@@ -144,9 +134,9 @@ public class Options extends Activity {
     	this.optionsNextLevel = b;
     	
     	if (b) {
-			imageButton4.setImageResource(R.drawable.button_yes);
+			imageButton3.setImageResource(R.drawable.button_yes);
     	} else {
-			imageButton4.setImageResource(R.drawable.button_no);
+			imageButton3.setImageResource(R.drawable.button_no);
     	}
     }
     
@@ -158,9 +148,9 @@ public class Options extends Activity {
     	this.optionsSound = b;
     	
     	if (b) {
-			imageButton1.setImageResource(R.drawable.button_sound_on);
+			imageButton1.setImageResource(R.drawable.button_yes);
     	} else {
-			imageButton1.setImageResource(R.drawable.button_sound_off);
+			imageButton1.setImageResource(R.drawable.button_no);
     	}
     }
 
