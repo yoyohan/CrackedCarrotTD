855d62bd5feb02d9f6f8842cb5a2f25bdad08ff8
Albin Andersson <albin.andersson@gmail.com>
2010-07-22 20:51:32 +0200

    small fix

--- src/com/crackedcarrot/NativeRender.java
+++ src/com/crackedcarrot/NativeRender.java
@@ -299,7 +299,7 @@ public class NativeRender implements GLSurfaceView.Renderer {
 
 				Iterator<TextureData> it = map.values().iterator();
 				while(it.hasNext()){
-					nativeFreeTex(it.next().mTextureName);
+					nativeFreeTex(it.next().texIndex);
 				}
 				lock2.release();
 			}
